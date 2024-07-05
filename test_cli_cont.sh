052ed0fc633ff87b36500e78a04d4a2e9e8bc6b5
Po <chenjunfeng@ethstorage.io>
2024-07-04 19:56:58 -0700

    add --cont depth comment

--- test_cli_cont.sh
+++ test_cli_cont.sh
@@ -13,7 +13,7 @@ WASM_FILE=$WKDIR/zkWasm/crates/zkwasm/wasm/fibonacci.wasm
 
 # Proving smoke_test of op-program client wasm image
 # RUST_LOG=info $ZKWASM --params ${WKDIR}/params fibonnaci setup -k 22
-RUST_LOG=info $ZKWASM --params ${WKDIR}/params fibonnaci prove  --output ${WKDIR}/output --wasm $WASM_FILE  --public 26:i64 --file
+# RUST_LOG=info $ZKWASM --params ${WKDIR}/params fibonnaci prove  --output ${WKDIR}/output --wasm $WASM_FILE  --public 26:i64 --file
 
 
 ## circuit batcher
@@ -25,4 +25,5 @@ BATCH_INFO_FINAL=$WKDIR/sample/cont-final.json
 cd $BATCHER_DIR
 # gen solidity failed, but proof generation succeeds
 # k must be 23, otherwise it'll throws not enough rows
+## --cont depth, 2^depth must be larger than total segements
 RUST_BACKTRACE=1 cargo run --features perf,cuda --release -- --param $WKDIR/params --output $WKDIR/output batch -k 23  -s shplonk --challenge sha --info $WKDIR/output/fibonnaci.loadinfo.json --name fib_agg --commits $BATCH_INFO_INIT $BATCH_INFO_RECT $BATCH_INFO_FINAL --cont 3
