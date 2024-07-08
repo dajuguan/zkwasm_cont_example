89b94044580da268de7e362a3a40aa04fd4f8bf4
Po <chenjunfeng@ethstorage.io>
2024-07-08 02:49:44 -0700

    add continuation script

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - Enable hugepage: `sudo sysctl -w vm.nr_hugepages=150000`
 
 ## Proving
-Run the following command to generate 1056 trace segments, each with 2 million WASM instructions, saved as files in the `output/traces` directory. Then, generate proofs for each segment, saved as files named `output/zkmain.{*}.transcript.data`.
+Run the following command to generate 1056 trace segments, each with 2 million WASM instructions, saved as files in the `output/traces` directory. Then, generate proofs for each segment, saved as files named `output/zkmain.{*}.transcript.data`. Finally, the aggregated proof is saved as file `output/zkmain_agg.final.0.transcript.data`. The total time cost is about 20 hours.
 
 ```
 bash test_cli_cont.sh
--- test_cli_cont.sh
+++ test_cli_cont.sh
@@ -8,10 +8,23 @@ WORKING_DIR=$PWD
 cd zkWasm
 cargo build --release --features perf,continuation,cuda
 
+
 ZKWASM=$WORKING_DIR/zkWasm/target/release/zkwasm-cli
 WASM_FILE=$WORKING_DIR/wasm/op-program-client-test.wasm
 PRIVATE_FILE=$WORKING_DIR/wasm/preimages-test.bin
+PROOF_NAME=zkmain
 
 # Proving smoke_test of op-program client wasm image
-RUST_LOG=info $ZKWASM --params ${WORKING_DIR}/params zkmain setup -k 23
-RUST_LOG=info $ZKWASM --params ${WORKING_DIR}/params zkmain prove  --output ${WORKING_DIR}/output --wasm $WASM_FILE  --private $PRIVATE_FILE:file --file
+# RUST_LOG=info $ZKWASM --params ${WORKING_DIR}/params ${PROOF_NAME} setup -k 23
+# RUST_LOG=info $ZKWASM --params ${WORKING_DIR}/params ${PROOF_NAME} prove  --output ${WORKING_DIR}/output --wasm $WASM_FILE  --private $PRIVATE_FILE:file --file
+
+
+# Aggregated proofs of zkWasm segments into one proof
+BATCHER_DIR=${WORKING_DIR}/continuation-batcher
+BATCHER=$BATCHER_DIR/target/release/circuit-batcher
+BATCH_INFO_INIT=$BATCHER_DIR/sample/cont-init.json
+BATCH_INFO_RECT=$BATCHER_DIR/sample/cont-rec.json
+BATCH_INFO_FINAL=$BATCHER_DIR/sample/cont-final.json
+
+cd $BATCHER_DIR
+RUST_BACKTRACE=1 cargo run --release --features perf,cuda -- --param ${WORKING_DIR}/params --output ${WORKING_DIR}/output batch -k 23 -s shplonk --challenge sha --info  ${WORKING_DIR}/output/${PROOF_NAME}.loadinfo.json --name ${PROOF_NAME}_agg --commits $BATCH_INFO_INIT $BATCH_INFO_RECT $BATCH_INFO_FINAL --cont 11
